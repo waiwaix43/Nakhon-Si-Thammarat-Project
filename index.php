0fa17c7dbd39434802c4712df78b6e790e1c7e18
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-02-18 01:48:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <!doctype html>
 <html lang="en">
   <head>
-     <!-- Required meta tags -->
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Chiang Mai Project</title>
+    <title>Hello, world!</title>
+    
     <style>
       #header{
-        height: 80px;
+        height: 160px;
         background-color: orange;
       }
       #sidebar{
         height: 450px;
-        background-color: #f1f1f1;
+        background-color: green;
         padding: 0px;
       }
       #content{
@@ -47,7 +48,7 @@
     
   </head>
   <body> 
-    <div class="header"> <!-- คอนเทนเนอร์ -->
+    <div class="container"> <!-- คอนเทนเนอร์ -->
       <div class="row">
         <div class="col-12" id="header"> <!-- เฮดเดอร์ -->
           <h1>Header</h1>
@@ -56,11 +57,10 @@
       <div class="row">
         <div class="col-3" id="sidebar"> <!-- ไซด์บาร์ -->
           <ul>
-            <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
-            <li><a href="https://lw-58513.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
-            <li><a href="https://lw-58513.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
-            <li><a href="https://lw-58513.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
-            <li><a href="https://lw-58513.herokuapp.com/page=4.php">ผู้จัดทำ</a></li> 
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
           </ul>
         </div>
         <div class="col-9" id="content"> <!-- คอนเท้นต์ -->
@@ -75,7 +75,5 @@
     </div>  
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
   </body>
 </html>
-
