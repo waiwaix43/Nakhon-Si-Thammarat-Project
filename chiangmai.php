99b184359f49c4380c4161d17e947b44be08ab7e
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-02-18 00:08:42 +0700

    Create chiangmai.php

---
+++ chiangmai.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <title>Navigation Bar</title>
+    <style>
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+      }
+      li {
+        float: left;
+      }
+      li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+      }
+      li a:hover {
+        background-color: #111;
+      }
+    </style>
+  </head>
+  <body>
+    <ul>
+      <li><a href="#">Coffee</a></li> 
+      <li><a href="#">Tea</a></li> 
+      <li><a href="#">Milk</a></li> 
+    </ul>
+  </body>
+</html>
