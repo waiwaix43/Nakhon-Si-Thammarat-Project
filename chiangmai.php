3b586aac97bd538b6a19127efb64073753885bc8
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-02-18 00:54:30 +0700

    Update chiangmai.php

--- chiangmai.php
+++ chiangmai.php
@@ -16,7 +16,7 @@
         display: block;
         color: white;
         text-align: center;
-        padding: 9px 16px;
+        padding: 12px 16px;
         text-decoration: none;
       }
       li a:hover {
