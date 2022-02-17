9e314339802ccac8b08fa2001482b6b1919541b0
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-02-18 01:04:05 +0700

    Create page=1.php

---
+++ page=1.php
@@ -0,0 +1,36 @@
+<html> 
+  <head>
+    <title>Chiang Mai Project</title>
+    <style>
+      ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+      }
+      li {
+        float: left;
+      }
+      li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 12px 16px;
+        text-decoration: none;
+      }
+      li a:hover {
+        background-color: #111;
+      }
+    </style>
+  </head>
+  <body>
+    <ul>
+      <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
+      <li><a href="#">แหล่งท่องเที่ยว</a></li> 
+      <li><a href="#">ประเพณีและวัฒนธรรม</a></li> 
+      <li><a href="#">อาหารประจำท้องถิ่น</a></li> 
+      <li><a href="#">ผู้จัดทำ</a></li> 
+    </ul>
+  </body>
+</html>
