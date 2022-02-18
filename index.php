1c959445c10a5c4e96eb93ec27433b3b7bcc5940
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-02-18 17:14:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Chonburi Project</title>
+    <title>Nakhon Si Thammarat Project</title>
     
     <style>
       #header{
@@ -51,7 +51,7 @@
     <div class="container"> <!-- คอนเทนเนอร์ -->
       <div class="row">
         <div class="col-12" id="header"> <!-- เฮดเดอร์ -->
-          <h1>จังหวัดชลบุรี</h1>
+          <h1>จังหวัดนครศรีธรรมราช</h1>
         </div>
       </div>
       <div class="row">
@@ -76,7 +76,7 @@
        </div>
         <div class="col-9" id="content"> <!-- คอนเท้นต์ -->
          <h4>
-           ชลบุรี
+           นครศรีธรรมราช
          </h4>
             <h5>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;เป็นจังหวัดในประเทศไทย มีประชากรมากที่สุดในภาคใต้และมีขนาดพื้นที่ใหญ่เป็นอันดับ 2 ของภาคใต้ (รองจากสุราษฎร์ธานี) เป็นจังหวัดที่มีอำเภอมากที่สุดในภาคใต้ ห่างจากกรุงเทพมหานคร ประมาณ 780 กิโลเมตร มีจังหวัดที่อยู่ติดกันได้แก่ สงขลา พัทลุง ตรัง กระบี่ และสุราษฎร์ธานี
