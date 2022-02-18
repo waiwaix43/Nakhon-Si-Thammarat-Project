96648dd6db088cdfe3a06201af165075c80c3363
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-02-19 00:23:58 +0700

    Update page=4.php

--- page=4.php
+++ page=4.php
@@ -1,36 +1,81 @@
-<html> 
+<!doctype html>
+<html lang="en">
   <head>
-    <title>Chiang Mai Project</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Nakhon Si Thammarat Project</title>
+    
     <style>
+      #header{
+        height: 120px;
+        background-color: #FFA500;
+      }
+      #sidebar{
+        height: 1800px;
+        background-color: #f1f1f1;
+        padding: 0px;
+      }
+      #content{
+        height: 1800px;
+        background-color: #F5F5F5;
+      }
+      #footer{
+        height: 200px;
+        background-color: #DCDCDC;
+      }
       ul {
         list-style-type: none;
         margin: 0;
         padding: 0;
-        overflow: hidden;
-        background-color: #333;
-      }
-      li {
-        float: left;
+        width: 100%;
+        background-color: #f1f1f1;
       }
       li a {
         display: block;
-        color: white;
-        text-align: center;
-        padding: 12px 16px;
+        color: #000;
+        padding: 8px 16px;
         text-decoration: none;
       }
+      /* Change the link color on hover */
       li a:hover {
-        background-color: #111;
+        background-color: #555;
+        color: #FFFFFF;
       }
     </style>
+    
   </head>
-  <body>
-    <ul>
-      <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
-      <li><a href="https://lw-58513.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
-      <li><a href="https://lw-58513.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
-      <li><a href="https://lw-58513.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
-      <li><a href="https://lw-58513.herokuapp.com/page=4.php">ผู้จัดทำ</a></li> 
-    </ul>
+  <body> 
+    <div class="container"> <!-- คอนเทนเนอร์ -->
+      <div class="row">
+        <div class="col-12" id="header"> <!-- เฮดเดอร์ -->
+          <h1>จังหวัดนครศรีธรรมราช</h1>
+          <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nakhon Si Thammarat</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3" id="sidebar"> <!-- ไซด์บาร์ -->
+          <ul>
+            <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
+            <li><a href="https://lw-58513.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
+            <li><a href="https://lw-58513.herokuapp.com/page=4.php">ผู้จัดทำ</a></li>
+          </ul>
+       </div>
+        <div class="col-9" id="content"> <!-- คอนเท้นต์ -->
+         
+        </div>
+      </div>
+       <div class="row">
+        <div class="col-12" id="footer"> <!-- ฟุตเตอร์ -->
+          <h1></h1>
+        </div>
+      </div>
+    </div>  
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
