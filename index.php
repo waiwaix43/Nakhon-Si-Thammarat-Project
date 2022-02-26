bc8749fde8513f5bf6cebab4329b02ca96460157
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-02-26 17:09:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 
     <style>
       #header{
-        width: 100%;
         height: 120px;
         background-color: #FFA500;
       }
@@ -21,7 +20,6 @@
       }
       #content{
         height:100%;
-        width: 100%;
         background-color: #F5F5F5;
       }
       ul {
