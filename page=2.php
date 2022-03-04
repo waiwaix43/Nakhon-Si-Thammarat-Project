0bba7474e2a966ab7737844c1166365f37360b70
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-03-04 14:41:34 +0700

    Update page=2.php

--- page=2.php
+++ page=2.php
@@ -1,25 +1,47 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
     <title>Nakhon Si Thammarat Project</title>
-    
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Sarabun"
+    />
     <style>
-      #header{
-        height: 120px;
-        background-color: #FFA500;
+      body {
+        font-family: "Sarabun";
+        background-color: #fdf2e7;
       }
-      #sidebar{
+      #container {
+        box-shadow: 6px 4px 8px 6px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      }
+      #header {
+        height: 160px;
+        background-image: url("P18.png");
+        background-size:100%;
+      }
+      #sidebar {
         background-color: #f1f1f1;
         padding: 0px;
       }
-      #content{
-        height:100%;
+      #content {
+        padding: 10px;
         background-color: #F5F5F5;
+      }
+      #footer {
+        height: 180px;
+        padding: 50px;
+        background-color: #DCDCDC;
+        border-radius: 0 0 15px 15px;
       }
       ul {
         list-style-type: none;
@@ -37,48 +59,42 @@
       /* Change the link color on hover */
       li a:hover {
         background-color: #555;
-        color: #FFFFFF;
+        color:#FFFFFF;
+      }
+      p{
+        margin-top: 10px;
       }
     </style>
-    
   </head>
-  <body style="background-color:#ffd3b2"> 
-    <div class="container"> <!-- คอนเทนเนอร์ -->
+  <body style="background-color:#ffd3b2">
+    <!-- คอนเทนเนอร์ -->
+    <div class="container">
+      <div id="container">
       <div class="row">
-        <div class="col-12" id="header"> <!-- เฮดเดอร์ -->
-          <table widht="1300" height="120" align="center">
-          <tr>
-           <td>
-             <img src="17.png">
-           </td> 
-          </tr>
-         </table>
+        <div class="col-12" id="header">
+          <!-- เฮดเดอร์ -->
         </div>
       </div>
       <div class="row">
-        <div class="col-3" id="sidebar"> <!-- ไซด์บาร์ -->
+        <div class="col-2" id="sidebar">
+          <!-- ไซด์บาร์ -->
           <ul>
             <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
             <li><a href="https://lw-58513.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
             <li><a href="https://lw-58513.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
             <li><a href="https://lw-58513.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
             <li><a href="https://lw-58513.herokuapp.com/page=4.php">ผู้จัดทำ</a></li>
-          </ul>
-       </div>
-        <div class="col-9" id="content"> <!-- คอนเท้นต์ -->
-        <h5>
-          ประเพณีและวัฒนธรรม
+         </ul>
+        </div>
+        <div class="col-10" id="content">
+          <!-- คอนเท้นต์ -->
+          <h4>
+           แหล่งท่องเที่ยวนครศรีธรรมราช
+         </h4>
+          1.ประเพณีแห่ผ้าขึ้นธาตุ
           </h5><br>
-          <h5>
-            1.ประเพณีแห่ผ้าขึ้นธาตุ
-          </h5><br>
-          <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="P7.png">
-           </td> 
-          </tr>
-         </table><br>
+         <center><img src="P7.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
 ในสมัยที่พระเจ้าศรีธรรมโศกราชเป็นกษัตริย์ครองตามพรลิงค์(นครศรีธรรมราช) อยู่นั้น ได้มีการบูรณะปฏิสังขรณ์พระบรมธาตุเจดีย์ครั้งใหญ่และแล้วเสร็จในปี พ.ศ.๑๗๗๓ 
 ขณะที่เตรียมสมโภชพระบรมธาตุอยู่นั้น ชาวปากพนังมากราบทูลว่า คลื่นได้ซัดเอาผ้าแถบยาวผืนหนึ่งซึ่งมีภาพเขียนเรื่องพุทธประวัติมาขึ้นที่ชายหาดปากพนัง ชาวปากพนังเก็บผ้านั้น
@@ -86,37 +102,27 @@
 จะนำผ้าพระบฏไปบูชาพระพุทธบาทที่ลังกา แต่ถูกพายุพัดพามาขึ้นชายฝั่งปากพนัง เหลือผู้รอดชีวิตสิบคนพระเจ้าศรีธรรมโศกราชทรงมีความเห็นว่าควรนำผ้าพระบฏไปห่มพระบรมธาตุเจดีย์
 เนื่องในโอกาสสมโภชพระบรมธาตุ แม้จะไม่ใช่พระพุทธบาทตามที่ตั้งใจ แต่ก็เป็นพระบรมสารีริกธาตุซึ่งเจ้าของผ้าพระบฏก็ยินดี การแห่ผ้าขึ้นธาตุจึงมีขึ้นตั้งแต่ปีนั้นและดำเนินการสืบต่อมา 
 จนกลายเป็นประเพณีสำคัญของชาวนครศรีธรรมราชในปัจจุบัน
-
 ระยะเวลาดำเนินการ
+            
 แต่เดิมการแห่ผ้าขึ้นธาตุนิยมจัดปีละสองครั้ง ในวันขึ้น ๑๕ ค่ำ เดือนสาม (วันมาฆบูชา) และวันขึ้น ๑๕ ค่ำ เดือนหก (วันวิสาขบูชา) โดยนำผ้าไปห่มองค์พระบรมธาตุเจดีย์ ณ วัดพระมหาธาตุวรมหาวิหาร ปัจจุบันนิยมทำกัน
 ในวันขึ้น ๑๕ ค่ำ เดือนสาม (วันมาฆบูชา) มากกว่า
-
-สาระสำคัญ			
+สาระสำคัญ
+            
 ๑. แสดงให้เห็นลักษณะสังคมของนครศรีธรรมราช ที่ยึดมั่นอยู่ในพระพุทธศาสนาการทำบุญเพื่ออุทิศเป็นพุทธบูชาเพื่อประสงค์ให้ใกล้ชิดกับพระพุทธเจ้า
 ๒. แสดงให้เห็นว่าองค์พระบรมธาตุเจดีย์ เป็นศูนย์รวมจิตใจ ศูนย์รวมความศรัทธา พุทธศาสนิกชนทั่วไปทุกทิศจึงประสงค์มาห่มผ้าพระธาตุอย่างพร้อมเพรียงกัน          </h5>
           <h5>
-            2.ลากพระบก
-          </h5><br>
-              <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="P8.png">
-           </td> 
-          </tr>
-         </table><br>
+          <h5>
+             2.ลากพระบก
+         <center><img src="P8.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
 ใช้เชือกแบ่งผูกเป็น 2 สาย เป็นสายผู้หญิงและสายผู้ชาย ใช้โพน ฆ้อง ระฆัง เป็นเครื่องตีให้จังหวะในการลากพระ คนลากจะเบียดเสียดกันสนุกสนานและประสาน เสียงร้องบทลากพระเพื่อผ่อนแรง 
 ตัวอย่างบทร้องที่ใช้ลากพระ คือ อี้สาระพา เฮโล เฮโล ไอ้ไหรกลมกลม หัวนมสาวสาว ไอ้ไหรยาวยาว สาวสาวชอบใจ            </h5>
           <h5>
             3.ประเพณีสารทเดือนสิบ
             </h5><br>
-              <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="p9.png">
-           </td> 
-          </tr>
-         </table><br>
+              <center><img src="P9.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
 พีธีกรรม เริ่มตั้งแต่วันแรม 1 ค่ำ เดือนสิบ ซึ่งถือว่าเป็นวันที่พญายมปล่อยตัวผู้ล่วงลับไปแล้วที่ (เรียกว่า "เปรต") มาจากนรก สำหรับวันนี้บางคนก็ประกอบพิธี บางคนจะประกอบพิธีใน
 วันแรม 13 ค่ำ 14 ค่ำ และ 15 ค่ำ โดยการนำอาหารไปทำบุญที่วัดเรียกว่า "หมรับเล็ก" เป็นการต้อนรับบรรพบุรุษ และญาติมิตรที่ขึ้นมาจากนรกเท่านั้น 
@@ -133,13 +139,8 @@
           <h5>
             4.ประเพณีให้ทานไฟ
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="p10.png">
-           </td> 
-          </tr>
-         </table><br>
+             <center><img src="P10.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
 นครศรีธรรมราชตั้งอยู่ติดชายฝั่งทะเล ลักษณะภูมิอากาศเป็นแบบร้อนชื้น ในหน้าหนาวก็ไม่หนาวจัด เพียงแต่คนรู้สึกว่าอากาศเย็นลงกว่าปกติ เนื่องจากไม่เคยชินกับอากาศที่หนาวเย็นลง ตอนย่ำรุ่งเช้ามืด
 จึงลุกขึ้นมาก่อไฟผิงเพื่อสร้างความอบอุ่น ประกอบกับชาวนครศรีธรรมราชยึดมั่นในพระพุทธศาสนา พุทธศาสนิกชนจึงพากันก่อกองไฟในวัดใกล้บ้าน แล้วนิมนต์พระภิกษุสงฆ์มาผิงไฟรับความอบอุ่นด้วย
@@ -152,21 +153,33 @@
           <h5>
             5.ลากพระน้ำ
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="P11.png">
-           </td> 
-          </tr>
-         </table><br>
+        <center><img src="P11.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
 การลากพระทางน้ำ หรือ “ลากพระน้ำ” ออกจะสนุกกว่า “ลากพระบก” เพราะสภาพการเอื้ออำนวยต่อกิจกรรมอื่น ๆ กว้างขวางกว่า เช่น สะดวกในการชักลาก ง่ายแก่การรวมกลุ่มกันจัดเรือพาย
 เพราะแต่ละกลุ่มมีลักษณะเป็นเอกเทศ ท้าทายต่อการแข่งขันประกวดประชันกันผนึกกำลังกันได้สะดวก มีกิจกรรมเชื่อมโยงอื่น ๆ ได้หลากหลายกว่า เช่น การแข่งพายเรือ การแย่งเรือพระ การเล่นเพลงเรือ 
 ความคิดเห็น
           </h5>
         </div>
-
+      </div>
+      <div class="row">
+        <div class="col-12" id="footer">
+          <!-- ฟุตเตอร์ -->
+          <center>
+            <h3>ผลงานนักเรียน</h3>
+            <hr />
+            <p>รายวิชา WebApplication</p>
+            <p>โรงเรียนหล่มสักวิทยาคม</p>
+          </center>
+        </div>
+      </div>
+      </div>  
+    </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
   </body>
 </html>
