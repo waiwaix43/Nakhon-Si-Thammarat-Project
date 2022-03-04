dbd1c9a546995171b2047300a5010c42c42bff09
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-03-04 14:37:26 +0700

    Update page=1.php

--- page=1.php
+++ page=1.php
@@ -1,27 +1,48 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
     <title>Nakhon Si Thammarat Project</title>
-    
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Sarabun"
+    />
     <style>
-      #header{
-        height: 120px;
-        background-color: #FFA500;
+      body {
+        font-family: "Sarabun";
+        background-color: #fdf2e7;
       }
-      #sidebar{
+      #container {
+        box-shadow: 6px 4px 8px 6px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      }
+      #header {
+        height: 160px;
+        background-image: url("P18.png");
+        background-size:100%;
+      }
+      #sidebar {
         background-color: #f1f1f1;
         padding: 0px;
       }
-      #content{
-        height:100%;
+      #content {
+        padding: 10px;
         background-color: #F5F5F5;
       }
-
+      #footer {
+        height: 180px;
+        padding: 50px;
+        background-color: #DCDCDC;
+        border-radius: 0 0 15px 15px;
+      }
       ul {
         list-style-type: none;
         margin: 0;
@@ -38,106 +59,97 @@
       /* Change the link color on hover */
       li a:hover {
         background-color: #555;
-        color: #FFFFFF;
+        color:#FFFFFF;
+      }
+      p{
+        margin-top: 10px;
       }
     </style>
-    
   </head>
-  <body style="background-color:#ffd3b2"> 
-    <div class="container"> <!-- คอนเทนเนอร์ -->
+  <body style="background-color:#ffd3b2">
+    <!-- คอนเทนเนอร์ -->
+    <div class="container">
+      <div id="container">
       <div class="row">
-        <div class="col-12" id="header"> <!-- เฮดเดอร์ -->
-         <table widht="1300" height="120" align="center">
-          <tr>
-           <td>
-             <img src="17.png">
-           </td> 
-          </tr>
-         </table>
+        <div class="col-12" id="header">
+          <!-- เฮดเดอร์ -->
         </div>
       </div>
       <div class="row">
-        <div class="col-3" id="sidebar"> <!-- ไซด์บาร์ -->
+        <div class="col-2" id="sidebar">
+          <!-- ไซด์บาร์ -->
           <ul>
             <li><a href="https://lw-58513.herokuapp.com/">หน้าแรก</a></li>
             <li><a href="https://lw-58513.herokuapp.com/page=1.php">แหล่งท่องเที่ยว</a></li> 
             <li><a href="https://lw-58513.herokuapp.com/page=2.php">ประเพณีและวัฒนธรรม</a></li> 
             <li><a href="https://lw-58513.herokuapp.com/page=3.php">อาหารประจำท้องถิ่น</a></li> 
             <li><a href="https://lw-58513.herokuapp.com/page=4.php">ผู้จัดทำ</a></li>
-          </ul>
-       </div>
-        <div class="col-9" id="content"> <!-- คอนเท้นต์ -->
-        <h5>
-          แหล่งท่องเที่ยวนครศรีธรรมราช
+         </ul>
+        </div>
+        <div class="col-10" id="content">
+          <!-- คอนเท้นต์ -->
+          <h4>
+           แหล่งท่องเที่ยวนครศรีธรรมราช
+         </h4>
+          1.หมู่บ้านคีรีวง
           </h5><br>
-          <h5>
-            1.หมู่บ้านคีรีวง
-          </h5><br>
-          <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="P2.png">
-           </td> 
-          </tr>
-         </table><br>
+          <center><img src="P2.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
             ชุมชนต้นแบบในการจัดการธุรกิจท่องเที่ยวเชิงนิเวศในจังหวัดนครศรีธรรมราช ที่มีวิถีชีวิตแบบชาวสวนอยู่กับธรรมชาติ และได้พัฒนา การบริการนักท่องเที่ยวขึ้นมาเป็นธุรกิจใหม่ของชุมชน จุดเด่นของหมู่บ้านคีรีวง ก็คือ ทัศนียภาพแห่งธรรมชาติ เพราะคีรีวงตั้งอยู่ท่ามกลางเทือกเขา ป่าไม้และสายน้ำ กิจกรรมที่น่าสนใจ ในหมู่บ้านคีรีวง คือ การพักในที่พักแบบโฮมสเตย์ เพลินตาและเพลินอารมณ์กับทัศนียภาพแห่ง ธรรมชาติ ที่ตั้งอยู่ท่ามกลางเทือกเขา ป่าไม้ ปั่นจักรยานชมวิวสูดอากาศบริสุทธิ์ ซึ่งสามารถหาเช่าจักรยานได้ตามร้านเช่าจักรยานในจุดต่างๆ รอบหมู่บ้าน
           </h5>
           <h5>
             2.เกาะนุ้ยนอก ขนอม
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="P3.png">
-           </td> 
-          </tr>
-         </table><br>
+              <center><img src="P3.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
             เกาะนุ้ยนอก  เกาะเล็กกลางทะเลในอำเภอขนอม จังหวัดนครศรีธรรมราช แต่มีสิ่งที่มหัศจรรย์บนเกาะ ทำให้เกาะแห่งนี้กลายเป็นสถานที่ Unseen เนื่องจากบนเกาะนี้มีบ่อน้ำจืดที่มีรูปร่างคล้ายกับรอยเท้า สามารถมองเห็นได้ในตอนที่น้ำทะเลลดลง ถ้าเป็นช่วงที่น้ำขึ้น บ่อน้ำนี้ก็จะถูกน้ำทะเลท่วมหมด เชื่อกันว่าพื้นที่แห่งนี้คือบริเวณที่เกิดตำนาน “หลวงปู่ทวดเหยียบน้ำทะเลจืด”  เกาะนุ้ยนอก มีทางบันไดเดินขึ้นไปก็สะดวกเพื่อไปไหว้หลวงปู่ทวด ข้างบนมีจุดชมวิว สามารถมองเห็นเกาะสลับซับซ้อน บริเวณรอบๆ หากไปช่วงที่น้ำลงสามารถเดินข้ามไปมาระหว่างเกาะได้อย่างสบาย  บนเกาะยังมีห้องน้ำไว้บริการนักท่องเที่ยวด้วย ถือเป็นหนึ่งในทะเลนครศรีธรรมราชที่ห้ามพลาดเลย
             </h5>
           <h5>
             3.ประตูระบายน้ำอุทกวิภาชประสิทธิ
             </h5><br>
-              <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="P4.png">
-           </td> 
-          </tr>
-         </table><br>
+              <center><img src="P4.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
             เป็นส่วนหนึ่งของโครงการพัฒนา พื้นที่ลุ่มน้ำปากพนังอันเนื่องมาจากพระราชดำริของพระบาทสมเด็จพระปรมินทรมหาภูมิพลอดุลยเดช  เพื่อช่วยเหลือบรรเทา ความทุกข์ยากเดือดร้อนของประชาชนในพื้นที่ลุ่มน้ำปากพนัง  มีสะพานข้ามฝั่งแม่น้ำเพื่อให้คนในพื้นที่ได้สัญจรข้ามไปมา ยังอีกฝั่ง และได้จัดทำเป็นสวนสาธารณะที่ร่มรื่นเหมาะสำหรับมาพักผ่อนชมวิว  จึงกลายเป็น สถานที่ท่องเที่ยวไฮไลท์ ที่น่าสนใจอีกแห่ง หนึ่งของอำเภอปากพนัง
           </h5>
           <h5>
             4.แหลมตะลุมพุก
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="P5.png">
-           </td> 
-          </tr>
-         </table><br>
+              <center><img src="P5.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
             แหลมตะลุมพุกอ.ปากพนัง มีชื่อเสียงเป็นที่รู้จักกันมายาวนาน เนื่องจากการถูกพายุพัดสร้างความเสียหาย ครั้งยิ่งใหญ่ที่สุดในประวัติศาสตร์   เป็นชายหาดสีขาวยาวโค้งเรียวยาวไปตามชายฝั่งทะเลแนวเหนือ-ใต้ ระยะทางประมาณ 6 กิโลเมตร ด้านหนึ่งของแหลมรับลมทะเล จากฝั่งอ่าวไทย อีกด้านรับคลื่นลมในฝั่งอ่าวปากแม่น้ำปากพนัง นักท่องเที่ยวสามารถเดินทางด้วยรถยนต์จนถึงปลายแหลมได้เป็น แหล่งท่องเที่ยวพักผ่อนหย่อนใจ ซึ่งในปัจจุบันมีการปรับทัศนียภาพให้สวยงามขึ้นเป็นแหล่งรวมร้านอาหาร ที่เสิร์ฟซีฟู้ดเคล้า วิวทะเลให้คุณได้อิ่มเอมอย่างเอร็ดอร่อย เป็นหนึ่งที่เที่ยวปากพนังที่ห้ามพลาดเด็ดขาด!
           </h5>
           <h5>
             5.กำแพงเมืองเก่า
           </h5><br>
-              <table widht="500" height="300" align="center">
-          <tr>
-           <td>
-             <img src="P6.png">
-           </td> 
-          </tr>
-         </table><br>
+              <center><img src="P6.png" class="img-fluid rounded"></center>
+          <br>
           <h5>
             กำแพงเมืองเก่า เป็น กำแพงเมืองที่เคยปกป้องเมืองนครศรีธรรมราชจากข้าศึก เป็นโบราณสถานที่สำคัญของจังหวัดแสดงถึงความเก่าแก่ ความแข็งแกร่ง ความเจริญรุ่งเรือง และประวัติศาสตร์อันยาวนาน ปัจจุบันได้รับการบูรณะใหม่ให้แข็งแรงและสวยงาม เพื่อพัฒนาเป็นอีก 1 สถานที่ท่องเที่ยวทางประวัติศาสตร์อันทรงคุณค่า มีลักษณะเป็นสี่เหลี่ยมผืนผ้า มีที่ตั้งอยู่หลายจุดด้วยกัน แต่ที่เหลือให้ชม คือ กำแพงเมืองด้านทิศเหนือ อยู่ริมคลอง หน้าเมือง ถนนมุมป้อม
           </h5>
         </div>
-
+      </div>
+      <div class="row">
+        <div class="col-12" id="footer">
+          <!-- ฟุตเตอร์ -->
+          <center>
+            <h3>ผลงานนักเรียน</h3>
+            <hr />
+            <p>รายวิชา WebApplication</p>
+            <p>โรงเรียนหล่มสักวิทยาคม</p>
+          </center>
+        </div>
+      </div>
+      </div>  
+    </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
   </body>
 </html>
