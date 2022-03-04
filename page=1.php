1b2ff8a1a5baafbcd399663acb2e128cb596e097
waiwaix43 <73818820+waiwaix43@users.noreply.github.com>
2022-03-04 15:05:03 +0700

    Update page=1.php

--- page=1.php
+++ page=1.php
@@ -25,7 +25,7 @@
         box-shadow: 6px 4px 8px 6px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
       }
       #header {
-        height: 100%;
+        height:160px;
         background-image: url("P18.png");
         background-size:100%;
       }
